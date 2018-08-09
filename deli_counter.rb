248f6d24129a829cafb663fb7a08bd35d0d88947
Kim Pham <pham.kim.a@gmail.com>
2018-08-09 04:39:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,8 @@ def line(array)
   if array.empty?
     puts "The line is currently empty."
   else
-    str = "The line is currently: "
-    array.each_index{|a| str+= "#{a+1}. " + array[a]}
+    str = "The line is currently:"
+    array.each_index{|a| str+= " #{a+1}. " + array[a]}
     puts str
   end
 end
