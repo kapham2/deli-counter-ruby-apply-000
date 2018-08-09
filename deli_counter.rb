7f368d29459a685ffe2908c44b228709337208fb
Kim Pham <pham.kim.a@gmail.com>
2018-08-09 03:09:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+katz_deli = []
+
+#def line()
+#  []
+#end
+
+def take_a_number(array, str)
+  array.push(str)
+  puts str
+end
+
+take_a_number(katz_deli,"Tommy")
+take_a_number(katz_deli,"Samba")
