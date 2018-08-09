b41fabe8b7f8100b43ea04a2fca272e9da9eaaf7
Kim Pham <pham.kim.a@gmail.com>
2018-08-09 03:50:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ katz_deli = []
 def line(array)
   print "The line is currently:"
   array.each{|a| print a + " "}
-  array.each_element{|a| print a + " "}
+  array.each_index{|a| print a + " "}
 end
 
 def take_a_number(array, str)
