169784723df864e02db44a19c9d2172a084c56d7
Kim Pham <pham.kim.a@gmail.com>
2018-08-09 03:58:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ katz_deli = []
 
 def line(array)
   if array.empty?
-    print "The line is currently empty."
+    puts "The line is currently empty."
   else
     print "The line is currently:"
     array.each_index{|a| print " #{a+1} " + array[a]}
